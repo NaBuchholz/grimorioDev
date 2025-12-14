cb52c7a4e40b612cda54b31a165a2e28a1d235bf
NaBuchholz <nathaliabuchh@hotmail.com>
2025-12-14 13:16:01 -0300

    fix: update page title in Quartz configuration

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🧙‍♀️ grimorioDev — Jardim Digital",
+    pageTitle: "Grimorio Dev",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
